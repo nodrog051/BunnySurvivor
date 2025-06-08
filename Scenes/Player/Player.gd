c27d4f4e5acf52dd693d6be58c83f5ca1c0c12f5
nodrog051 <gordon.dickson@gmail.com>
2025-06-08 13:43:38 +0100

    Added gravity

---
+++ Scenes/Player/Player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+class_name Player
+const GRAVITY: float = 600.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	move_and_slide()
